2b5e75d3c952064c880f6e5d10a483b3713ba32f
Élise <atomheartother@gmail.com>
2017-06-08 14:24:06 +0200

    Started adding wolves

--- wolves.js
+++ wolves.js
@@ -1,8 +1,4 @@
 const wolves = {
-    "Momiji" : {
-        "svg" : true,
-        "tags" : ["Touhou", "Awoo~"]
-    },
     "Holo" : {
         "svg" : false,
         "tags" : ["Spice and Wolf", "Ookami to Koushinryou"]
@@ -10,5 +6,33 @@ const wolves = {
     "Karen" : {
         "svg" : false,
         "tags" : ["Kiniro Mosaic", "Karen Kujou", "Ookami to Koushinryou"]
-    }
+    },
+    "Madoka" : {
+        "svg" : false,
+        "tags" : ["Puella Magi Madoka Magica", "Meguca", "Madoka Kaname"]
+    },
+    "Marisa" : {
+        "svg" : false,
+        "tags" : ["Touhou", "Marisa Kirisame"]
+    },
+    "Megumin" : {
+        "svg" : false,
+        "tags" : ["Konosuba", "Kono Subarashii Sekai ni Shukufuku wo!", "Explosion!"]
+    },
+    "Momiji" : {
+        "svg" : true,
+        "tags" : ["Touhou", "Awoo~"]
+    },
+    "Muffet" : {
+        "svg" : false,
+        "tags" : ["Undertale", "Afufu~"]
+    },
+    "Papyrus" : {
+        "svg" : false,
+        "tags" : ["Undertale", "Nyeh nyeh nyeh"]
+    },
+    "Ryuko" : {
+        "svg" : false,
+        "tags" : ["Kill La Kill", "Ryuko Matoi"]
+    },
 };
